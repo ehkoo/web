aa5b89d82904d3013b650cc6cba51c130601f54d
An Cao <kcj.pop.com@gmail.com>
2017-12-05 09:11:39 +0200

    Add a FB box in the middle of the post
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- assets/global.js
+++ assets/global.js
@@ -3,6 +3,16 @@
   window.onload = () => {
     stickybits('.sticky', { noStyles: true })
     // zoom image
-    if (window.zoom != null) window.zoom(document.querySelector('#js-content img'))
+    const images = document.querySelector('#js-content img')
+    if (window.zoom != null && images) window.zoom(images)
+
+    // Insert FB like box in the middle of content
+    const fbBox = document.getElementById('js-fb-box')
+    const ps = document.querySelectorAll('#js-content p')
+    if (fbBox && ps) {
+      const parent = document.getElementById('js-content')
+      const archor = ps[Math.ceil(ps.length / 2)]
+      parent.insertBefore(fbBox, archor)
+    }
   }
 })()
