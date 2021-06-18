5c5d073953aed39b75db98bfdc0f3e1248ebec08
An Cao <kcj.pop.com@gmail.com>
2021-06-18 12:07:22 +0300

    Fix undefined toggler

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,24 @@
 window.onload = function ehkoo() {
   function toggleMobileNav() {
     const toggler = document.getElementById('js-toggle-mobile-nav')
-    const up = toggler.querySelector('i[data-icon="up"]')
-    const down = toggler.querySelector('i[data-icon="down"]')
-    const nav = document.getElementById('js-mobile-nav')
 
-    toggler.addEventListener(
-      'click',
-      e => {
-        e.preventDefault()
-        up.classList.toggle('hidden')
-        down.classList.toggle('hidden')
-        nav.classList.toggle('hidden')
-        document.body.classList.toggle('overflow-hidden')
-      },
-      false,
-    )
+    if (toggler) {
+      const up = toggler.querySelector('i[data-icon="up"]')
+      const down = toggler.querySelector('i[data-icon="down"]')
+      const nav = document.getElementById('js-mobile-nav')
+
+      toggler.addEventListener(
+        'click',
+        e => {
+          e.preventDefault()
+          up.classList.toggle('hidden')
+          down.classList.toggle('hidden')
+          nav.classList.toggle('hidden')
+          document.body.classList.toggle('overflow-hidden')
+        },
+        false,
+      )
+    }
   }
 
   function toggleToc() {
