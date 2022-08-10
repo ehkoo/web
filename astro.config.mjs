791ff5b6a889945002505b777f175fc4130647b9
An Cao <kcj.pop.com@gmail.com>
2022-08-10 08:33:52 +0300

    Upgrade to Astro 1.0.1
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://ehkoo.com',
-  server: { port: 6001, host: false },
+  server: {
+    port: 6001,
+    host: false
+  },
   markdown: {
     shikiConfig: {
-      theme: 'github-dark-dimmed',
-    },
+      theme: 'github-dark-dimmed'
+    }
   },
-})
+  integrations: [mdx()]
+});
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss'
+
+export const get = async () => {
+  const posts = import.meta.glob('./bai-viet/**/*.md', { eager: true })
+
+  return rss({
+    title: 'Ehkoo',
+    description:
+      'Website tiếng Việt chuyên hướng dẫn tùm lum về JavaScript, CSS, React, và chuyện lập trình frontend.',
+    customData: `<language>vi</language>`,
+    site: import.meta.env.SITE,
+    items: Object.values(posts).map((item) => ({
+      title: item.frontmatter.title,
+      description: item.frontmatter.description,
+      link: item.url,
+      pubDate: item.frontmatter.date,
+    })),
+  })
+}
